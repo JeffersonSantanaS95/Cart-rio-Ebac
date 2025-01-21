ba50e9d56e9af98acf6c02bcdf578c9e7129772c
JeffersonSantanaS95 <jefferson.santanatheo@gmail.com>
2025-01-21 20:58:17 -0300

    Alteração opção de saída
    Incluso no sistema a opção de saída do usuário.

--- CartorioEBAC.c
+++ CartorioEBAC.c
@@ -115,6 +115,7 @@ int main()
 	printf ("\t1 - Registrar nomes \n");
 	printf ("\t2 - Consultar nomes \n");
 	printf ("\t3 - Deletar nomes \n\n"); 
+	printf ("\t4 - sair do sistema \n\n");
 	printf ("Opção:");
 	
 	scanf ("%d", &opcao); //armazenando a escolha do usuario
@@ -135,6 +136,12 @@ int main()
 			case 3:
 			deletar();
 		break;
+		
+		case 4:
+			printf("Obrigado por usar o sistema");
+			return 0;
+			break;
+
 		
 
 		
